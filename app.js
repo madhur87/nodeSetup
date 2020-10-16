2d1e213a292a5471c966ec31079cd756378760ba
Madhur Rastogi <rastogi.madhur87@gmail.com>
2020-10-16 17:40:24 +0530

    Update app.js

--- app.js
+++ app.js
@@ -5,6 +5,10 @@ app.get('/',(req,res)=>{
   res.json({msg : "Hello World"});
 };
 
+app.get('/new',(req,res)=>{
+  res.status(200).json({msg : "Hello World With Status"});
+};
+
 app.listin(3000,()=>{
  console.log("server started");
 };
